2ec0944ecfd652677eab78081e47a9d719ae471c
neeteshgolhani <neeteshgolhani4@gmail.com>
2023-07-12 03:24:03 +0530

    Added:C:Create a Helloworld Spring App to display “Hello from BridgeLabz” in REST Call

---
+++ src/main/java/com/bridgelabz/helloworld/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.helloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+    @RequestMapping("/api")
+    public class HelloWorldController {
+        @GetMapping("/hello")
+        public String helloWorld() {
+            return "Hello from BridgeLabz";
+        }
+}
